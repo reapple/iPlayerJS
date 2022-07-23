fc4b01e1d1baafe881a023e54299dc50f862db3e
reapple <45746666+reapple@users.noreply.github.com>
2022-07-23 18:19:19 +0800

    Update liveBox.js

--- liveBox.js
+++ liveBox.js
@@ -16,7 +16,13 @@ async function getPage1() {
         timeout : 16
     }
     iNetwork.get(options, function(err, res, body){
-        page1Data = body.data
+        if (version >= '1.5.0') {
+            let obj = JSON.parse(body)
+            page1Data = obj.data
+        }else {
+            page1Data = body.data
+        }
+        
         let data = {
             title: '平台列表',
             canPlay: false,
@@ -32,7 +38,7 @@ async function getPage1() {
                 }
 
             },
-            data: body.data
+            data: page1Data
         }
 
         iUI.reloadData(data)
@@ -49,6 +55,12 @@ async function getPage2(index) {
         timeout : 16
     }
     iNetwork.get(options, function(err, res, body){
+        let obj = []
+        if (version >= '1.5.0') {
+            obj = JSON.parse(body)
+        }else {
+            obj = body.data
+        }
         let data = {
             title: subData['name'],
             canPlay: true,
@@ -58,11 +70,19 @@ async function getPage2(index) {
                     image:'img',
                     address: 'address'
                 }
-
             },
-            data: body.data
+            data: obj
         }
 
         iUI.reloadData(data)
     })
 }
+
+//版本号
+function version() {
+    try {
+        return iUI.appVersion()
+    }catch(e){}
+    return '1.0.0'
+}
+
